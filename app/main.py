132e7d2e131b5c26dc8583ed5802562c8f5731af
OneSummerDay <artem.kraievyi@gmail.com>
2026-03-16 19:58:17 +0200

    add application settings config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
 from app.api.v1.main_routes import router as main_router
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+    debug=settings.debug,
+)
 
 app.include_router(main_router, prefix="/api/v1")
