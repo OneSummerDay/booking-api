cc2a38016c788afe95a04213ed22825b75d08c43
OneSummerDay <artem.kraievyi@gmail.com>
2026-03-16 20:31:57 +0200

    add database session dependency

--- app/core/database.py
+++ app/core/database.py
@@ -16,3 +16,8 @@ async_session_maker = async_sessionmaker(
 
 class Base(DeclarativeBase):
     pass
+
+
+async def get_db_session():
+    async with async_session_maker() as session:
+        yield session
